4e02d16c27cbe360d7e4ad4f59e576a4d7e8678d
bassaroff <bassarov_01@mail.ru>
2021-10-11 18:49:23 +0600

    adding new widget focused contextmenu, redesign home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,36 +9,83 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _value = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const HomeAppBar(),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Align(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  'Restaurants',
-                  style: Theme.of(context).textTheme.headline2,
+      body: Container(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "Restaraunts",
+                      style: TextStyle(
+                          fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                    Expanded(child: Center()),
+                    IconButton(
+                      icon: Icon(Icons.add_shopping_cart), onPressed: () => {},)
+                  ],
                 ),
-              ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    DropdownButton(
+                      value: _value,
+                      style: TextStyle(fontSize: 15, color: Colors.black),
+                      icon: Icon(Icons.keyboard_arrow_down),
+                      underline: Container(
+                        color: Colors.white,
+                      ),
+                      items: [
+                        DropdownMenuItem(
+                          child: Text("All"),
+                          value: 1,
+                        ),
+                        DropdownMenuItem(
+                          child: Text("Open"),
+                          value: 2,
+                        ),
+                        DropdownMenuItem(
+                          child: Text("Closed"),
+                          value: 3,
+                        ),
+                      ],
+                      onChanged: (int? value){
+                        setState(() {
+                          _value = value!;
+                        });
+                      },
+                      hint: Text("Select"),
+                    ),
+                    IconButton(onPressed: () => {}, icon: Icon(Icons.sort))
+                  ],
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    physics: BouncingScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: Restaurant.restaurants.length,
+                    itemBuilder: (context, index) {
+                      return RestaurantCard(
+                        restaurant: Restaurant.restaurants[index],
+                      );
+                    },
+                  ),
+                )
+              ],
             ),
-            ListView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: Restaurant.restaurants.length,
-              itemBuilder: (context, index) {
-                return RestaurantCard(
-                  restaurant: Restaurant.restaurants[index],
-                );
-              },
-            ),
-          ],
-        ),
+          ),
       ),
     );
   }
@@ -63,7 +110,8 @@ class HomeAppBar extends StatelessWidget with PreferredSizeWidget {
         children: [
           Text(
             'Almaty',
-            style: Theme.of(context)
+            style: Theme
+                .of(context)
                 .textTheme
                 .headline3!
                 .copyWith(color: Colors.white),
