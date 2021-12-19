b288552c0b3c58f4874138ff8800fbf6fd35970c
abosyaka <56767281+abosyaka@users.noreply.github.com>
2021-12-19 21:06:30 +0600

    added search functional (#1)

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,5 +1,11 @@
+import 'package:eat_meat/models/menu_item.dart';
+import 'package:eat_meat/models/restaurant.dart';
+import 'package:eat_meat/pages/restaurant_details_screen.dart';
+import 'package:eat_meat/util/db_handler.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:focused_menu/focused_menu.dart';
+import 'package:focused_menu/modals.dart';
 
 class Search extends StatefulWidget {
   const Search({Key? key}) : super(key: key);
@@ -10,6 +16,17 @@ class Search extends StatefulWidget {
 
 class _SearchState extends State<Search> {
   final textController = TextEditingController();
+  var allRestaurants = [];
+  var items = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    DbHandler.instance.getRestaurants().then((value) => {
+          setState(() => {allRestaurants = value, items = allRestaurants})
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,14 +54,105 @@ class _SearchState extends State<Search> {
                 padding: const EdgeInsets.symmetric(vertical: 15),
                 prefixInsets: const EdgeInsets.all(15),
                 suffixInsets: const EdgeInsets.all(15),
+                onChanged: (value) {
+                  setState(() {
+                    filterSearch(value);
+                  });
+                },
               ),
-              Scrollbar(
-                child: Container(),
-              ),
+              Expanded(
+                  child: ListView(
+                      physics: const BouncingScrollPhysics(),
+                      shrinkWrap: true,
+                      children: items.map(
+                        (restaurant) {
+                          restaurant.tags = const [
+                            'Pizza',
+                            'Sushi',
+                            'Burger',
+                            'Pasta',
+                          ];
+                          restaurant.menuItems = MenuItem.menuItems
+                              .where((element) => element.restaurantId == 1)
+                              .toList();
+                          return FocusedMenuHolder(
+                            child: RestaurantCard(
+                              restaurant: restaurant,
+                            ),
+                            blurSize: 5.0,
+                            menuItemExtent: 45,
+                            menuWidth: MediaQuery.of(context).size.width * 0.5,
+                            menuBoxDecoration: const BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(15.0),
+                              ),
+                            ),
+                            duration: const Duration(milliseconds: 100),
+                            animateMenuItems: true,
+                            blurBackgroundColor: Colors.black54,
+                            menuOffset: 0,
+                            bottomOffsetHeight: 80.0,
+                            menuItems: [
+                              FocusedMenuItem(
+                                title: const Text("Add to favourites"),
+                                trailingIcon:
+                                    const Icon(Icons.control_point_outlined),
+                                onPressed: () => {},
+                              ),
+                              FocusedMenuItem(
+                                title: const Text("Share"),
+                                trailingIcon: const Icon(Icons.share),
+                                onPressed: () => {},
+                              ),
+                              FocusedMenuItem(
+                                title: const Text("Review"),
+                                trailingIcon: const Icon(Icons.comment),
+                                onPressed: () => {},
+                              ),
+                            ],
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => RestaurantDetailsScreen(
+                                    restaurant: restaurant,
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        },
+                      ).toList())),
             ],
           ),
         ),
       ),
     );
+  }
+
+  filterSearch(String value) {
+    var dummySearchList = allRestaurants;
+    if (value.isNotEmpty) {
+      dummySearchList = [];
+      for (var element in allRestaurants) {
+        Restaurant restaurant = element;
+        if (restaurant.name.toLowerCase().contains(value.toLowerCase())) {
+          dummySearchList.add(element);
+        }
+      }
+
+      setState(() {
+        items = [];
+        items.addAll(dummySearchList);
+      });
+
+      return;
+    } else {
+      setState(() {
+        items = [];
+        items = allRestaurants;
+      });
+    }
   }
 }
